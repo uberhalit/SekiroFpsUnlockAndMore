037508716735c7a11eef54d636bd8a30f0379b25
Marcelo Formentão <marceloavf3@hotmail.com>
2021-01-04 07:49:42 -0300

    chore: update assembly version

--- SekiroFpsUnlockAndMore/Properties/AssemblyInfo.cs
+++ SekiroFpsUnlockAndMore/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("1.2.5.2")]
-[assembly: AssemblyFileVersion("1.2.5.2")]
+[assembly: AssemblyVersion("1.2.5.3")]
+[assembly: AssemblyFileVersion("1.2.5.3")]
